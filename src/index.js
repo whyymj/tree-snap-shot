6b248e97954eacf5a1ddff0f5dbfbc53d1792cbf
武浩 <wuhao1@haier.com>
2021-08-05 17:12:27 +0800

    feat: 创建项目

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import {
+    objectDiff
+} from './diff/objectDiff.js'
+import {
+    myersDiffHandler as arrayDiff
+} from './diff/arrayDiff.js'
+import Immutable from 'immutable'
+import isObject from 'isobject'
+import {
+    getDataType
+} from './util/index'
+
+function differs(data1, data2, path, type, resultObj, parents, handler) {
+    if (Array.isArray(data1) && Array.isArray(data2)) {
+        arrayDiff(data1, data2, path, type, resultObj, parents, handler);
+    } else if (getDataType(data1) == 'Immutable List' && getDataType(data2) == 'Immutable List') {
+        arrayDiff(data1.toJS(), data2.toJS(), path, type, resultObj, parents, handler);
+    } else if (isObject(data1) || isObject(data2)) {
+        objectDiff(data1, data2, path, type, resultObj, parents, handler);
+    }
+}
+
+function diff(data1, data2, path = []) {
+    let result = [];
+    differs(data1, data2, Immutable.List([]), Immutable.List([]), result, (path.length ? Immutable.List(path) : null), differs)
+    return Immutable.fromJS(result).toJS();
+}
+
+(function (global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
+        typeof define === 'function' && define.amd ? define(['exports'], factory) :
+        (global = typeof globalThis !== 'undefined' ? globalThis : global || self, factory(global.TreeDiff = {}));
+}(this, (function (exports) {
+    var TreeDiff = {
+        diff
+    }
+    exports.default = TreeDiff;
+    exports.diff = diff
+}))) 
---
+++ test/index.test.js
@@ -0,0 +1,45 @@
+import {
+  diff
+} from '../dist/index'
+
+
+test('diff([1, 2, 4, 3], [0, 1, 2, 6, 5, 3, 4])', () => {
+  expect(diff([1, 2, 4, 3], [0, 1, 2, 6, 5, 3, 4])).toEqual([{
+    "path": [],
+    "type": [],
+    "operation": "diff",
+    "value": [{
+      "operation": "add",
+      "value": 0,
+      "index": [0, 0]
+    }, {
+      "operation": "",
+      "value": 1,
+      "index": [0, 1]
+    }, {
+      "operation": "",
+      "value": 2,
+      "index": [1, 2]
+    }, {
+      "operation": "del",
+      "value": 4,
+      "index": [2, 3]
+    }, {
+      "operation": "add",
+      "value": 6,
+      "index": [3, 3]
+    }, {
+      "operation": "add",
+      "value": 5,
+      "index": [3, 4]
+    }, {
+      "operation": "",
+      "value": 3,
+      "index": [3, 5]
+    }, {
+      "operation": "add",
+      "value": 4,
+      "index": [4, 6]
+    }]
+  }]);
+});
